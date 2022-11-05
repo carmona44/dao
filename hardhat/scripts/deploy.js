a2e2d75dbc037f3f52c6a916fc2a0a3cbcf08eac
carmona44 <dcacarmona44@gmail.com>
2022-11-05 19:02:33 +0100

    RESOLVE: add frontend

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -8,7 +8,6 @@ async function main() {
     await fakeNftMarketplace.deployed();
 
     console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
-    //0x29955F19C6c6a3d8b2995689cfFE481320c245dc
 
     const VodkaDAOContract = await ethers.getContractFactory("VodkaDAO");
     const vodkaDAO = await VodkaDAOContract.deploy(fakeNftMarketplace.address, BUENACHICA_NFT_CONTRACT_ADDRESS, { value: ethers.utils.parseEther("0.1") });
